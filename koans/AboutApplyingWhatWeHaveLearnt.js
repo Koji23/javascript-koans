474f1be8e97856f8d17a0fd3a10f5441b4a0e58d
Jordan <jordanchong23@gmail.com>
2016-04-20 20:38:47 -0700

    "Summing Multiples of 3 and 5, Imperitive & Functional"

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -57,13 +57,18 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
     
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    var sum = _.range(1000).reduce(function(memo, item){
+      if(item % 3 === 0 || item % 5 === 0){
+        return memo + item;
+      }
+      return memo;
+    });    /* try chaining range() and reduce() */
 
-    expect(233168).toBe(FILL_ME_IN);
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
